459e630d23dda425abe16d67648a808f3745d7f8
Remik <remik.ratajski@gmail.com>
2022-05-19 20:48:25 +0200

    Fix tests

--- app/src/androidTest/java/com/example/note/activities/MainActivityTest.java
+++ app/src/androidTest/java/com/example/note/activities/MainActivityTest.java
@@ -1,6 +1,8 @@
 package com.example.note.activities;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
@@ -8,32 +10,33 @@ import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.note.R;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    @Test
-    public void testActivityInView() {
-        ActivityScenario.launch(MainActivity.class);
-
-        onView(withId(R.layout.activity_main)).check(matches(isDisplayed()));
-    }
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
     public void testEmptyListIsVisible() {
-        ActivityScenario.launch(MainActivity.class);
+        openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+        onView(withText(R.string.delete_all)).perform(click());
+        onView(withId(R.id.textDeleteNote)).perform(click());
 
         onView(withId(R.id.empty_text)).check(matches(isDisplayed()));
         onView(withId(R.id.empty_text)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
-        onView(withId(R.id.searchInput)).check(matches(withText(R.string.empty)));
+        onView(withId(R.id.empty_text)).check(matches(withText(R.string.empty)));
 
         onView(withId(R.id.empty_image)).check(matches(isDisplayed()));
         onView(withId(R.id.empty_image)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
@@ -41,8 +44,6 @@ public class MainActivityTest {
 
     @Test
     public void testSearchInput() {
-        ActivityScenario.launch(MainActivity.class);
-
         onView(withId(R.id.searchInput)).check(matches(isDisplayed()));
         onView(withId(R.id.searchInput)).check(matches(withHint(R.string.search_input_hint)));
     }
